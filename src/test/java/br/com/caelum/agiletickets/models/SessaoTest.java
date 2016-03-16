0b6016fb8e7dea22bcdcbebbc71aa35b839f715b
gustavo martins <gustavo.marinelli@gmail.com>
2016-03-16 19:33:47 -0300

    Erro na classe Sessao: Não é possível reservar o ultimo ingresso

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,5 +1,7 @@
 package br.com.caelum.agiletickets.models;
 
+import static org.junit.Assert.*;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -36,6 +38,16 @@ public class SessaoTest {
 
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
+	}
+	
+	@Test
+	public void deveReservarUltimoIngresso() throws Exception {
+		
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(1);
+		
+		Assert.assertTrue(sessao.podeReservar(1));
+		
 	}
 	
 }
